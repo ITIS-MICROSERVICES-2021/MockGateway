2aab2220607f1536189d1a9c5c106b0913fa3a93
Grigorev Yakov <49405484+JumpyL1on@users.noreply.github.com>
2021-09-29 21:58:57 +0300

    Получение со стороны данных о пользователях (#2)
    * Create ExternalUserDto.cs
    
    * Create IExternalUserService.cs
    
    * Create ExternalUserService.cs
    
    * Create ExternalUserController.cs
    
    * Update Startup.cs

---
+++ MockGateway/Controllers/ExternalUserController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MockGateway.Interfaces;
+
+namespace MockGateway.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ExternalUserController : ControllerBase
+    {
+        private IExternalUserService ExternalUserService { get; }
+
+        public ExternalUserController(IExternalUserService externalUserService)
+        {
+            ExternalUserService = externalUserService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await ExternalUserService.GetExternalUser();
+            return Ok(result);
+        }
+    }
+}
