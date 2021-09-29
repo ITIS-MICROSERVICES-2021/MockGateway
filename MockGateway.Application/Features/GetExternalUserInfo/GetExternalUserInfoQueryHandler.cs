69c96b4b475c28bd54c269052dddb337ea406147
Grigorev Yakov <yaschok315@gmail.com>
2021-09-29 13:34:51 +0300

    Create GetExternalUserInfoQueryHandler.cs

---
+++ MockGateway.Application/Features/GetExternalUserInfo/GetExternalUserInfoQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using MockGateway.Domain.ValueObjects;
+
+namespace MockGateway.Application.Features.GetExternalUserInfo
+{
+    public class GetExternalUserInfoQueryHandler : IRequestHandler<GetExternalUserInfoQuery, ExternalUserDto>
+    {
+        private IMapper Mapper { get; }
+
+        public GetExternalUserInfoQueryHandler(IMapper mapper)
+        {
+            Mapper = mapper;
+        }
+
+        public async Task<ExternalUserDto> Handle(GetExternalUserInfoQuery request, CancellationToken cancellationToken)
+        {
+            var externalUser = new ExternalUser
+            {
+                Surname = "Ivanov",
+                Name = "Ivan",
+                Patronymic = "Ivanovich",
+                Position = "Software Engineer",
+                ManagerFullName = "Petrov Petr Petrovich",
+                Salary = 30000,
+                RecruitmentDate = new DateTime(2021, 9, 29)
+            };
+            return Mapper.Map<ExternalUser, ExternalUserDto>(externalUser);
+        }
+    }
+}
