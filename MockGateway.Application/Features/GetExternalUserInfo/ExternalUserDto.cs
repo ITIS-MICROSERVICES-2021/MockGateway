b06cd61d4977c76ab13b4fbc8445cd2843113fb9
Grigorev Yakov <yaschok315@gmail.com>
2021-09-29 13:02:47 +0300

    added external user dto

---
+++ MockGateway.Application/Features/GetExternalUserInfo/ExternalUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MockGateway.Application.Features.GetExternalUserInfo
+{
+    public record ExternalUserDto
+    {
+        public string Surname { get; init; }
+        public string Name { get; init; }
+        public string Patronymic { get; init; }
+        public string Position { get; init; }
+        public string ManagerFullName { get; init; }
+        public long Salary { get; init; }
+        public DateTime RecruitmentDate { get; init; }
+    }
+}
