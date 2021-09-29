d1dc62312fd22b48504cc935ea9b1c5c28268f1d
Grigorev Yakov <yaschok315@gmail.com>
2021-09-29 13:42:00 +0300

    added external user controller

---
+++ MockGateway.WebAPI/Controllers/ExternalUserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MockGateway.Application.Features.GetExternalUserInfo;
+
+namespace MockGateway.Domain.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ExternalUserController : ControllerBase
+    {
+        private IMediator Mediator { get; }
+
+        public ExternalUserController(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetExternalUser()
+        {
+            var query = new GetExternalUserInfoQuery();
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
