6028e1d82fb388bd65690367e57a46ae4cb15755
Grigorev Yakov <yaschok315@gmail.com>
2021-09-29 13:38:09 +0300

    Create MappingProfile.cs

---
+++ MockGateway.Application/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MockGateway.Application.Features.GetExternalUserInfo;
+using MockGateway.Domain.ValueObjects;
+
+namespace MockGateway.Application
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ExternalUser, ExternalUserDto>();
+        }
+    }
+}
