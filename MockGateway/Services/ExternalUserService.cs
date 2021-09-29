0ddf072208c5ae7d06b15c9e34a0b22d38cd2d29
Grigorev Yakov <yaschok315@gmail.com>
2021-09-29 21:53:13 +0300

    Create ExternalUserService.cs

---
+++ MockGateway/Services/ExternalUserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using MockGateway.DTOs;
+using MockGateway.Interfaces;
+
+namespace MockGateway.Services
+{
+    public class ExternalUserService : IExternalUserService
+    {
+        public async Task<ExternalUserDTO> GetExternalUser()
+        {
+            return new ExternalUserDTO
+            {
+                Surname = "Иванов",
+                Name = "Иван",
+                Patronymic = "Иванович",
+                Position = "Программист",
+                ManagerFullName = "Петров Петр Петрович",
+                CompanyFullName = "Предприятие",
+                Salary = 30000,
+                RecruitmentDate = DateTime.Now
+            };
+        }
+    }
+};
