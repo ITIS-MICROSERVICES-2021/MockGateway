3f8d3510b0b6b5f14df3006b00ff286e42543fa9
Grigorev Yakov <yaschok315@gmail.com>
2021-09-29 12:48:44 +0300

    Create ExternalUser.cs

---
+++ MockGateway.Domain/ValueObjects/ExternalUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MockGateway.Domain.ValueObjects
+{
+    public record ExternalUser
+    {
+        public string Surname { get; init; }
+        public string Name { get; init; }
+        public string Patronymic { get; init; }
+        public string Position { get; init; }
+        public string ManagerFullName { get; init; }
+        public long Salary { get; init; }
+        public DateTime RecruitmentDate { get; init; }
+    }
+}
